b29484c6b1ea1d19cd989cfffcf74671cb646bc2
Aniket <aniket.d2402@gmail.com>
2024-01-28 19:32:15 -0500

    added code to show star rating

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+
+import StarRating from './StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating maxRating={5} />
+    <StarRating size={24} color='green' maxRating={8} />
   </React.StrictMode>
 );
